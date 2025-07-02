85c3431dfbdf2e36609cde473761439b44105c31
pauldvlp <paulbh.dev@gmail.com>
2025-07-02 15:55:45 -0600

    feat(vercel): add support for analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { App } from '@/App'
 import '@/index.css'
 import '@/styles/hugeicons.css'
 import '@fontsource-variable/bricolage-grotesque/index.css'
+import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider } from 'next-themes'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <ThemeProvider enableSystem={false}>
       <App />
     </ThemeProvider>
