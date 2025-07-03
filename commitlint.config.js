499b924aeaf725bb6df6ea91a4fbcedf90bfb559
pauldvlp <paulbh.dev@gmail.com>
2025-07-03 00:56:27 -0600

    🐳 chore: add new type-enums to commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,17 @@
-export default { extends: ['git-commit-emoji'] }
+import gitCommitEmoji from 'commitlint-config-git-commit-emoji'
+
+const { rules } = gitCommitEmoji
+
+export default {
+  extends: ['git-commit-emoji'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        '🔖 chore',
+        ...rules['type-enum'].pop()
+      ]
+    ]
+  }
+}
